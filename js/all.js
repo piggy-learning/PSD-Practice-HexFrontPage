b265557d1210f700a03b0f37051f0460831c8f68
wfox5510 <wfox5510@gmail.com>
2024-05-02 21:33:33 +0800

    add goTop button

--- js/all.js
+++ js/all.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
     event.preventDefault();
     $(this).find('ul').stop().slideToggle(200);
   });
+  $('.goTop').click(function (e) { 
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: 0
+    }, 600);
+  });
+  
 });
+
